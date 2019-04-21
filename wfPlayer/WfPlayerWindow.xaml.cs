caf3197fe92d0e6c4564d805c105fb0b3b8a1fac
toyota-m2k <mitsuki.toyota@metamoji.com>
2019-04-22 01:00:37 +0900

    first commit

---
+++ wfPlayer/WfPlayerWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wfPlayer
+{
+    /// <summary>
+    /// WfPlayerWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class WfPlayerWindow : Window
+    {
+        public WfPlayerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMediaOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnMediaEnded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnMediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
