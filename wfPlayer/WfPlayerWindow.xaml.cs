a421bded4f30caf83f1af71ea1954a9e59788662
toyota-m2k <mitsuki.toyota@metamoji.com>
2019-06-28 20:05:30 +0900

    remocon2-2

--- wfPlayer/WfPlayerWindow.xaml.cs
+++ wfPlayer/WfPlayerWindow.xaml.cs
@@ -1149,6 +1149,11 @@ namespace wfPlayer
             System.Windows.Forms.Cursor.Position = new System.Drawing.Point(0, 0);
         }
 
+        private void ShutdownPC() {
+            Close();
+            Application.Current.Shutdown();
+        }
+
         #endregion
 
         #region Trimming
@@ -1246,7 +1251,8 @@ namespace wfPlayer
             public const string TRIM_SELECT = "trimSelect";
             public const string TRIM_RESET = "trimReset";
 
-            public const string KICKOUT_MOUSE = "kickoutMouse";
+            public const string KICKOUT_MOUSE = "kickOutMouse";
+            public const string SHUTDOWN = "shutdown";
         }
 
         private Dictionary<System.Windows.Input.Key, string> mKeyCommandMap = null;
@@ -1281,6 +1287,7 @@ namespace wfPlayer
                 { Commands.TRIM_RESET, ()=>ResetTrimming(Current as WfFileItem) },
 
                 { Commands.KICKOUT_MOUSE, KickOutMouse },
+                { Commands.SHUTDOWN, ShutdownPC }
             };
 
             mKeyCommandMap = new Dictionary<Key, string>()
